c8cbbb6163562b16fa5fee8444ac7ddc29bc1d39
Daniil Titov <dt@emsoft.ru>
2021-04-17 15:32:55 +0300

    add cqrs request logger

---
+++ MyBlog.Services/Common/RequestLogger.cs
@@ -0,0 +1,30 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyBlog.Services.Common
+{
+    public class RequestLogger<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+
+        public RequestLogger(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var name = typeof(TRequest).Name;
+
+            _logger.LogInformation($"Request: {name} | {request}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
