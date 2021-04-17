c1ba7675e1978c5443cbc87bbc0f7ad50df96164
Daniil Titov <dt@emsoft.ru>
2021-04-17 15:34:05 +0300

    markup for new-blogs change

--- MyBlogFront/src/app/main/new-blogs/new-blogs.component.ts
+++ MyBlogFront/src/app/main/new-blogs/new-blogs.component.ts
@@ -2,17 +2,22 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Blog} from '../../models/blog';
 
 @Component({
-  selector: 'app-new-blogs',
-  templateUrl: './new-blogs.component.html',
-  styleUrls: ['./new-blogs.component.scss'],
+    selector: 'app-new-blogs',
+    templateUrl: './new-blogs.component.html',
+    styleUrls: ['./new-blogs.component.scss'],
 })
 export class NewBlogsComponent implements OnInit {
 
-  @Input() latestBlogs: Blog[] = [];
+    @Input() public newestBlogs: Blog[] = [];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    public errorImageHandler(event: any): void {
+        console.log(typeof (event));
+        event.target.src = `../../assets/error-images/preview-error.jpg`;
+    }
 }
