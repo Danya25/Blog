3f3434062f688b7415a0a99b9d2344b6876dabfe
Daniil Titov <dt@emsoft.ru>
2021-04-17 15:33:12 +0300

    migration for short description blog

---
+++ MyBlog.DAL/Migrations/20210414135253_Add Short Description for blog.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyBlog.DAL.Migrations
+{
+    public partial class AddShortDescriptionforblog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShortDescription",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShortDescription",
+                table: "Blogs");
+        }
+    }
+}
