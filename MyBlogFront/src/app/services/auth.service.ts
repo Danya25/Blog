f6f09704d8202904d4b87b9e97c43a19e3870526
Daniil Titov <dt@emsoft.ru>
2021-05-16 12:49:26 +0300

    add method IsAuthenticated auth serivce

--- MyBlogFront/src/app/services/auth.service.ts
+++ MyBlogFront/src/app/services/auth.service.ts
@@ -4,13 +4,15 @@ import {User} from '../models/user';
 import {Observable} from 'rxjs';
 import {Answer} from '../models/answer';
 import {UserInfo} from '../models/userInfo';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {tap} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {
     }
 
     public login(user: User): Observable<Answer<UserInfo>> {
@@ -18,6 +20,12 @@ export class AuthService {
     }
 
     public registration(user: User): Observable<Answer<any>> {
-        return this.http.post<Answer<any>>('apu/Auth/Registration', user);
+        return this.http.post<Answer<any>>('api/Auth/Registration', user);
+    }
+
+
+    public IsAuthenticated(): boolean {
+        const token = localStorage.getItem('token');
+        return !this.jwtHelper.isTokenExpired(token);
     }
 }
