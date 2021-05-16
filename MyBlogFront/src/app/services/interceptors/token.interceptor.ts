a68ccf658a149bc0f04613e799a3155e07331bfe
Daniil Titov <dt@emsoft.ru>
2021-05-16 12:59:22 +0300

    jwt library automaticly add jwt token into requests

--- MyBlogFront/src/app/services/interceptors/token.interceptor.ts
+++ MyBlogFront/src/app/services/interceptors/token.interceptor.ts
@@ -1,25 +1,23 @@
 import {Injectable} from '@angular/core';
-import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor
-} from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-    constructor() {
+    constructor(private jwtService: JwtHelperService, private route: Router) {
     }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        const token = localStorage.getItem('user') === null ? null : JSON.parse(localStorage.getItem('user')).token;
-        const req = request.clone({
-            setHeaders: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-        return next.handle(req);
+        const token = localStorage.getItem('token');
+
+        if (this.jwtService.isTokenExpired(token)) {
+            localStorage.clear();
+            this.route.navigate(['/']);
+        }
+        
+        return next.handle(request);
     }
 }
