d3718dc7c08528b2bfd73857f23fba826636f2cc
Daniil Titov <dt@emsoft.ru>
2021-05-16 13:04:51 +0300

    admin layout

--- MyBlogFront/src/app/layouts/admin-layout/admin-layout.component.ts
+++ MyBlogFront/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RoleService} from '../../services/role.service';
+import {RolesQuery} from '../../session/queries/roles.query';
+import {filter, switchMap} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-admin-layout',
-  templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+    selector: 'app-admin-layout',
+    templateUrl: './admin-layout.component.html',
+    styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent implements OnInit {
 
-  constructor() { }
+    constructor(private roleService: RoleService, private roleQuery: RolesQuery) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+
+    }
 
 }
