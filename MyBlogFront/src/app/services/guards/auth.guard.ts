cf73f1668658d432c2c12d21195105b5a47afb6e
Daniil Titov <dt@emsoft.ru>
2021-05-16 12:57:41 +0300

    auth/unauth guard correct

--- MyBlogFront/src/app/services/guards/auth.guard.ts
+++ MyBlogFront/src/app/services/guards/auth.guard.ts
@@ -2,21 +2,23 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {UserService} from '../user.service';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {AuthService} from '../auth.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-    constructor(private route: Router) {
+    constructor(private route: Router, private auth: AuthService) {
     }
 
     canActivate(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        if (localStorage.getItem('user')) {
+        if (this.auth.IsAuthenticated()) {
             return true;
         } else {
-            this.route.navigate(['/auth/login']);
+            this.route.navigate(['/']);
             return false;
         }
     }
