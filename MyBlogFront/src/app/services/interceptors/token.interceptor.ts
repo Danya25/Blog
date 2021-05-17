4a307fde50ff601aba339dc547951b2278f5b0fc
Daniil Titov <dt@emsoft.ru>
2021-05-17 10:17:56 +0300

    token checked before expired

--- MyBlogFront/src/app/services/interceptors/token.interceptor.ts
+++ MyBlogFront/src/app/services/interceptors/token.interceptor.ts
@@ -13,7 +13,7 @@ export class TokenInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         const token = localStorage.getItem('token');
 
-        if (this.jwtService.isTokenExpired(token)) {
+        if (token !== null && this.jwtService.isTokenExpired(token)) {
             localStorage.clear();
             this.route.navigate(['/']);
         }
