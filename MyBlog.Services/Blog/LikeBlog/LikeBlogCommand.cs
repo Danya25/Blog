c19ef4122dcbef1f1e573a41160a4d20a496eddb
Daniil Titov <dt@emsoft.ru>
2021-06-06 14:36:46 +0300

    cqrs like blog

---
+++ MyBlog.Services/Blog/LikeBlog/LikeBlogCommand.cs
@@ -0,0 +1,17 @@
+﻿using MyBlog.Domain.Commands;
+
+namespace MyBlog.Services.Blog.LikeBlog
+{
+    public class LikeBlogCommand : CommandBase<bool>
+    {
+        public int UserId { get; }
+        public int BlogId { get; }
+        public bool LikeStatus { get; }
+        public LikeBlogCommand(int userId, int blogId, bool likeStatus)
+        {
+            UserId = userId;
+            BlogId = blogId;
+            LikeStatus = likeStatus;
+        }
+    }
+}
---
+++ MyBlog.Services/Blog/LikeBlog/LikeBlogHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MyBlog.DAL;
+using MyBlog.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyBlog.Services.Blog.LikeBlog
+{
+    public class LikeBlogHandler : ICommandHandler<LikeBlogCommand, bool>
+    {
+        private readonly ApplicationContext _context;
+
+        public LikeBlogHandler( ApplicationContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Handle(LikeBlogCommand request, CancellationToken cancellationToken)
+        {
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    if (!request.LikeStatus)
+                    {
+                        await _context.BlogLikes.AddAsync(new DAL.Entity.BlogLike { BlogId = request.BlogId, UserId = request.UserId });
+                    }
+                    else
+                    {
+                        _context.BlogLikes.Remove(new DAL.Entity.BlogLike { BlogId = request.BlogId, UserId = request.UserId });
+                    }
+                    await _context.SaveChangesAsync();
+                    await transaction.CommitAsync();
+                    return true;
+                }
+                catch
+                {
+                    await transaction.RollbackAsync();
+                    return false;
+                }
+            }
+        }
+    }
+}
