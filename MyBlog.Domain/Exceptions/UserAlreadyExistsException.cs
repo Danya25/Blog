d2f2a9e5e2b4892ae3e01c640b84c1bb03bfc7d8
Daniil Titov <dt@emsoft.ru>
2021-06-06 14:37:57 +0300

    new exceptions add

---
+++ MyBlog.Domain/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Domain.Exceptions
+{
+    public class UserAlreadyExistsException : Exception
+    {
+        public UserAlreadyExistsException()
+        {
+
+        }
+        public UserAlreadyExistsException(string message) : base(message)
+        {
+
+        }
+        public UserAlreadyExistsException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
