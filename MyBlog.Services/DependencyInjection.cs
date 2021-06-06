4253136692ea8d4b9e36fefbc6f8c45013d70e35
Daniil Titov <dt@emsoft.ru>
2021-06-06 14:36:35 +0300

    DI for Services

---
+++ MyBlog.Services/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using MyBlog.Services.Common;
+using System;
+using System.Reflection;
+
+namespace MyBlog.Services
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            return services;
+        }
+    }
+}
