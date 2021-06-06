740a1fa29c7fdab34082830314d51ca261b8370a
Daniil Titov <dt@emsoft.ru>
2021-06-06 14:39:46 +0300

    Middleware for catch exceptions

---
+++ MyBlog/Middleware/ApiExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyBlog.Common;
+
+namespace MyBlog.Middleware
+{
+    public class ApiExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ApiExceptionHandlingMiddleware> _logger;
+
+        public ApiExceptionHandlingMiddleware(RequestDelegate next, ILogger<ApiExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            _logger.LogError(ex, $"An unhandled exception has occurred, {ex.Message}");
+            var problemDetails = ex.ToErrorMethodResult<bool>();
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var result = JsonSerializer.Serialize(problemDetails);
+
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
